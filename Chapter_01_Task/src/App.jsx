c6c056dd21ee9f034e576a3b3dc0e539c48c679c
Suraj Yadav <sarry7045@gmail.com>
2025-10-23 13:28:20 +0530

    Chapter_01

---
+++ Chapter_01_Task/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { welcome } from './welcome.jsx'
+
+function App() {
+
+  return (
+    <>
+      {welcome()}
+    </>
+  )
+}
+
+export default App
